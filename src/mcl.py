728947f0d402d3f013d90f5114478b389c1a3cf8
Henry Laih <henrylaih41@yahoo.com>
2018-06-01 23:27:42 +0800

    modified graph_clustering and added get_Gateway

--- src/mcl.py
+++ src/mcl.py
@@ -5,24 +5,22 @@ from cmty import buildG
 import sys
 import matplotlib.pyplot as plt
 
-def main(argv):
-    if len(argv) < 2:
-        sys.stderr.write("Usage: %s <input graph>\n" % (argv[0],))
-        return 1
-    graph_fn = argv[1]
-    G = nx.Graph()  #let's create the graph first
-    buildG(G, graph_fn, ',')
-    n_Matrix = nx.to_scipy_sparse_matrix(G)
-    result = mc.run_mcl(n_Matrix,inflation=1.1)
+# Cluster_rate determine how big the cluster are. Recommend value: 1.1 ~ 1.9
+# 
+def graph_clustering(graph,cluster_rate = 1.5, draw = False):
+    AS_Num = 0
+    n_Matrix = nx.to_scipy_sparse_matrix(graph)
+    result = mc.run_mcl(n_Matrix,inflation = cluster_rate)
     clusters = mc.get_clusters(result)
-    print(clusters)
     print("Number of clusters: " + str(len(clusters)))
+    graph.graph['Total_AS'] = len(clusters)
     for c in clusters:
-        print(len(c))
-    mc.draw_graph(n_Matrix, clusters,
-    node_size=10, with_labels=False, edge_color="black",width=0.2)
-    plt.show()
-    
+        for n_id in c:
+            graph.add_node(n_id,AS_N = AS_Num)
+        AS_Num += 1
+    if(draw):
+        mc.draw_graph(n_Matrix, clusters,
+        node_size=10, with_labels=False, edge_color="black",width=0.2)
+        plt.show()
 
-if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+
--- src/proj.py
+++ src/proj.py
@@ -1,9 +1,21 @@
 import manager as m
 import networkx as nx
 import project_utility as p
+import mcl as mcl
+import matplotlib.pyplot as plt
 G = nx.Graph()
 p.buildG(G,"2018-5-30-69nodes_links.csv")
-print(G.nodes())
+mcl.graph_clustering(graph = G,cluster_rate=1.2)
+p.get_gateway(G)
+print(G.nodes(data=True))
+print(G.graph)
+colors = ['r','g','b','y','black','pink','gray']
+color_map = ['black'] * len(G)
+for n in G.nodes():
+    print(n)
+    color_map[n] = colors[G.node[n]['AS_N']]
+nx.draw_networkx(G,node_size = 10,width = 0.2,node_color = color_map,font_size = 5)
+plt.show()
 """g = m.Manager()
 g.set_Configuration(layerNum = 4,layerNodeNum = [20,500,1250,3600], outputPath = "../connection_data/", 
 conPara = {"1,1": 150,"1,2": 230,"2,2": 350,"2,3": 60,"3,3":60,"3,4":10,"4,4":16},
